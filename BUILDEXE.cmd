3c6eb7a12e3a2d4781b1513ed6309c3c3a4e406f
semool <semool@protonmail.com>
2026-02-15 15:30:27 +0100

    trim BUILDEXE.cmd

--- BUILDEXE.cmd
+++ BUILDEXE.cmd
@@ -1,14 +1,5 @@
-::go install github.com/akavel/rsrc@latest
-::go mod tidy
-
-::rsrc -manifest main.manifest -o main.syso
-::rsrc -manifest main.manifest -ico icon.ico -o main.syso
+go install github.com/akavel/rsrc@latest
+go mod tidy
 rsrc -manifest main.manifest -ico icon.ico
-
-:: Build under Windows for Windows
 go build -trimpath -gcflags "-l -B" -ldflags="-s -w" -o MSIAfterburnerProfileSwitcherDebug.exe
 go build -trimpath -gcflags "-l -B" -ldflags="-s -w -H windowsgui" -o MSIAfterburnerProfileSwitcher.exe
-
-:: Build under Linux for Windows
-::GOOS=windows GOARCH=amd64 go build -trimpath -gcflags "-l -B" -ldflags="-s -w" -o MSIAfterburnerProfileSwitcherDebug.exe
-::GOOS=windows GOARCH=amd64 go build -trimpath -gcflags "-l -B" -ldflags="-s -w -H windowsgui" -o MSIAfterburnerProfileSwitcher.exe
